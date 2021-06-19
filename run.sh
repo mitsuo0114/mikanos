93f05194fac1b1bf886ce77fe6f825cb3461b6c0
Takahiro Ono <oono0114@gmail.com>
2021-06-19 10:25:05 +0900

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
+source /mnt/d/workspaces/os/osbook/devenv/buildenv.sh
+
 pushd ../kernel
-clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp || {
+clang++ $CPPFLAGS -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp || {
  echo "Failed in compile kernel"
  exit 1
 }
 
-ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o || {
+ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o || {
  echo "Failed in link kernel"
  exit 1
 }
